73d927bdde74060b6897f1c5798b156281329146
Aren <88814386+Aren-666@users.noreply.github.com>
2021-08-15 22:18:26 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 $('.menu-btn').on('click',function(){
   $('.menu-content').toggleClass('open');
+  $('.menu-btn').toggleClass('btn-open');
 });
 $('.menu-list').on('click',function(){
   $('.menu-content').removeClass('open');
+  $('.menu-btn').removeClass('btn-open');
 });
